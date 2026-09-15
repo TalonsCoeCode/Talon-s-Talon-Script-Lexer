f3c67381369af0ad9f44edbe887e8a10e0e39eb7
TalonsCoeCode <tmsargent24@coe.edu>
2026-09-15 08:32:48 -0500

    Hate This Application

--- TestLexer.java
+++ TestLexer.java
@@ -1,2 +1,18 @@
 public class TestLexer {
+    public static void main(String[] args) {
+
+        String code = """
+            print("Hello");
+            x = 123;
+            // this is a comment
+            y = x + 5;
+        """;
+
+        Lexer lexer = new Lexer(code);
+        var tokens = lexer.getTokens();
+
+        for (Token t : tokens) {
+            System.out.println(t);
+        }
+    }
 }
