ee97a3cb5cb6606d291be33632badff3d11f0393
TalonsCoeCode <tmsargent24@coe.edu>
2026-09-22 09:54:17 -0500

    Update Token.java

--- src/Token.java
+++ src/Token.java
@@ -1,1 +1,19 @@
+public class Token {
+    final String lexeme;
+    final Object literal;
+    final int line;
+    final TokenType type;
+
+    public Token(String lexeme, Object literal, int line, TokenType type) {
+        this.lexeme = lexeme;
+        this.literal = literal;
+        this.line = line;
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return type + " " + lexeme + " " + literal + " (line " + line + ")";
+    }
+}
 
